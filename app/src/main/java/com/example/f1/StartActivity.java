80e4c3f84a9cb19482bf74bf1a7d083fd793a60f
RS <rushiji999@gmail.com>
2022-09-08 15:34:00 -0400

    fully working registration of a user

---
+++ app/src/main/java/com/example/f1/StartActivity.java
@@ -0,0 +1,40 @@
+package com.example.f1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        register = findViewById(R.id.Register);
+        login = findViewById(R.id.Login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
